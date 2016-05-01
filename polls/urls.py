374dbc4d8a5d33f954ab9fefab0c944e2fd70f96
Veronika <lspdv@users.noreply.github.com>
2016-05-01 15:05:37 +0200

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,6 @@ from polls import views
 app_name = 'polls'
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.IndexView.as_view(), name='index'),
